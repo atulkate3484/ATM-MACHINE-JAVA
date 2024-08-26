a60c6a945577a6bdc83ce3bbe32ffcc5b350b1fd
Atul Tatyaso kate <akate3484@gmail.com>
2024-08-26 23:45:17 +0530

    Add files via upload

---
+++ ATM/LoginPage.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package loginpage;
+
+/**
+ *
+ * @author Atul
+ */
+public class LoginPage {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Login log=new Login();
+        log.show();
+    }
+    
+}
